d2c50fd2068c19e5cc06eabf1741327f9c2d1f31
Cephas Armstrong-Mensah <carmstr7@asu.edu>
2018-03-29 01:47:33 -0700

    adding more external jar

--- src/server/gui/EmotivComposer.java
+++ src/server/gui/EmotivComposer.java
@@ -2,41 +2,56 @@ package server.gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.websocket.DeploymentException;
+import javax.websocket.server.ServerEndpoint;
 
 public class EmotivComposer extends JFrame {
 
-	private JPanel contentPane;
+  private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					EmotivComposer frame = new EmotivComposer();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    org.glassfish.tyrus.server.Server server = new org.glassfish.tyrus.server.Server("localhost", 10001,
+        "/ws", ServerEndpoint.class);
+    try {
+      server.start();
+      System.out.println("Press any key to stop the server..");
+      new Scanner(System.in).nextLine();
+    } catch (DeploymentException e) {
+      throw new RuntimeException(e);
+    } finally {
+      server.stop();
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public EmotivComposer() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          EmotivComposer frame = new EmotivComposer();
+          frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  /**
+   * Create the frame.
+   */
+  public EmotivComposer() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 450, 300);
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    contentPane.setLayout(new BorderLayout(0, 0));
+    setContentPane(contentPane);
+  }
 
 }
--- src/server/sys/ServerWebSocket.java
+++ src/server/sys/ServerWebSocket.java
@@ -1,8 +1,31 @@
 package server.sys;
 
+import java.io.IOException;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import data.EmotivData;
 
 @ServerEndpoint(value = "")
 public class ServerWebSocket {
+
+  @OnOpen
+  public void onOpen(Session session) {
+  }
+
+  @OnMessage
+  public void onMesage(EmotivData emotivData, Session session) throws IOException, EncodeException {
+
+  }
+
+  @OnClose
+  public void onClose() throws IOException, EncodeException {
+
+  }
 
 }
