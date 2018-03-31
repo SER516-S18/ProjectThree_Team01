80e76e2706d5862dab944deeed9982d124abc624
Cephas Armstrong-Mensah <cephas@asu.edu>
2018-03-30 19:39:56 -0700

    making construtor private for singleton pattern

--- src/client/gui/EmotivControlPanel.java
+++ src/client/gui/EmotivControlPanel.java
@@ -32,7 +32,7 @@ public class EmotivControlPanel extends JFrame {
   /**
    * Create the frame.
    */
-  public EmotivControlPanel() {
+  private EmotivControlPanel() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBounds(100, 100, 450, 300);
     contentPane = new JPanel();
