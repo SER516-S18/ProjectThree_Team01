7a23bd8eed103f81ba60ffa777d6675fdb431ad0
janu2207 <jahnavib22@gmail.com>
2018-03-30 18:10:13 -0700

    TG-9 #in-progress, Created tab for facial expression and panel for chart

--- src/client/gui/EmotivControlPanel.java
+++ src/client/gui/EmotivControlPanel.java
@@ -1,16 +1,20 @@
 package client.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.border.EmptyBorder;
 
-public class EmotivContorlPanel extends JFrame {
+import org.jfree.chart.ChartPanel;
 
-	private JPanel contentPane;
+public class EmotivControlPanel extends JFrame {
 
+  private JPanel contentPane;
+  private JPanel facialExpressionPanel ;
 	/**
 	 * Launch the application.
 	 */
@@ -18,7 +22,7 @@ public class EmotivContorlPanel extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					EmotivContorlPanel frame = new EmotivContorlPanel();
+					EmotivControlPanel frame = new EmotivControlPanel();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +34,23 @@ public class EmotivContorlPanel extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public EmotivContorlPanel() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	public EmotivControlPanel() {
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setBounds(100, 100, 450, 300);
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    setContentPane(contentPane);
+    contentPane.setLayout(null);
+    facialExpressionPanel = new JPanel();
+    facialExpressionPanel.setLayout(null);
+    JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+    tabbedPane.setBounds(5, 5, 422, 243);
+    contentPane.add(tabbedPane);
+    tabbedPane.setName("Facial Expressions");
+    tabbedPane.addTab("Facial Expressions", facialExpressionPanel);
+    JPanel graphPanel = new JPanel();
+    graphPanel.setBounds(199, 0, 218, 213);
+    facialExpressionPanel.add(graphPanel);
 	}
 
 }
