2795f54d0fd19a0c3b4cdedc7429235d0e255510
Cephas Armstrong-Mensah <cephas@asu.edu>
2018-04-02 01:32:26 -0700

    minor commenting changes, changing block comments to javadoc style /** */ and not /* */.

--- src/client/gui/DisplayGraph.java
+++ src/client/gui/DisplayGraph.java
@@ -1,4 +1,5 @@
 package client.gui;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,108 +10,89 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.DomainOrder;
-import org.jfree.data.general.DatasetChangeListener;
-import org.jfree.data.general.DatasetGroup;
-import org.jfree.data.time.Millisecond;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-
-/*This panel represents the graph panel
- * It contains the graph, dataset and other chart related parameters
+/**
+ * This panel represents the graph panel It contains the graph, dataset and
+ * other chart related parameters
  * 
  */
 
 public class DisplayGraph extends JPanel {
+  private static final long serialVersionUID = 6070705829173054591L;
+
   JFreeChart graph;
   int channel;
   int value;
-  XYSeriesCollection dataset ;
+  XYSeriesCollection dataset;
   public XYSeries series;
   public XYSeries graphSeries[];
-  ChartPanel chartPanel  ;
+  ChartPanel chartPanel;
   ArrayList<Integer> time = new ArrayList<Integer>();
-  
-  static DisplayThread c ;
 
+  static DisplayThread c;
 
   public DisplayGraph() {
     super();
-    graphSeries= new XYSeries[11];
-    dataset= new XYSeriesCollection();
-    for(int i=0;i<11;i++)
-    {
-    	int channelno=i+1;
-    graphSeries[i] = new XYSeries("Channel"+channelno);
-    dataset.addSeries(graphSeries[i]);
+    graphSeries = new XYSeries[11];
+    dataset = new XYSeriesCollection();
+    for (int i = 0; i < 11; i++) {
+      int channelno = i + 1;
+      graphSeries[i] = new XYSeries("Channel" + channelno);
+      dataset.addSeries(graphSeries[i]);
     }
-    graph= createChart(dataset);
-
+    graph = createChart(dataset);
 
   }
-  /*
+
+  /**
    * This function creates the chart with necessary parameters.
    */
   private JFreeChart createChart(final XYDataset dataset) {
-    final JFreeChart result = ChartFactory.createTimeSeriesChart(
-        "Plot", 
-        "Time", 
-        "Value",
-        dataset, 
-        true, 
-        true, 
-        false
-        );
+    final JFreeChart result = ChartFactory.createTimeSeriesChart("Plot", "Time", "Value", dataset, true,
+        true, false);
     final XYPlot plot = result.getXYPlot();
     ValueAxis axis = plot.getDomainAxis();
     axis.setAutoRange(true);
     axis.setFixedAutoRange(60000.0);
-  //  axis.setRange(0.0,1.0); 
+    // axis.setRange(0.0,1.0);
 
     axis = plot.getRangeAxis();
     return result;
   }
 
-  /*
+  /**
    * This method starts the thread each time a value is received
    */
-
-  public void updateGraph(int channel,int value){
-    this.channel=channel;
-    this.value=value;
+  public void updateGraph(int channel, int value) {
+    this.channel = channel;
+    this.value = value;
     if (c == null) {
       c = new DisplayThread();
       new Thread(c).start();
     }
-    
 
   }
 
-  /*
+  /**
    * This thread adds each value to the graph dynamically.
    */
   public class DisplayThread implements Runnable {
-	  private volatile boolean exit=false;
+    private volatile boolean exit = false;
+
     @Override
     public void run() {
       while (!exit) {
-    	  
- 
-				for(int i=0;i<channel;i++)
-				{
-						Random random = new Random();
-					
-				
-						graphSeries[i].add(random.nextInt(), random.nextInt());
-						dataset.getSeries(i).add(random.nextInt(), random.nextInt());
-					
-					
-					
-    	        }
+
+        for (int i = 0; i < channel; i++) {
+          Random random = new Random();
+
+          graphSeries[i].add(random.nextInt(), random.nextInt());
+          dataset.getSeries(i).add(random.nextInt(), random.nextInt());
+
+        }
         try {
           Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -118,11 +100,9 @@ public class DisplayGraph extends JPanel {
         }
       }
     }
-    public void stop()
-    {
-    	exit=true;
+
+    public void stop() {
+      exit = true;
     }
-
   }
-
 }
--- src/client/sys/ClientThread.java
+++ src/client/sys/ClientThread.java
@@ -3,28 +3,27 @@ package client.sys;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.websocket.ClientEndpoint;
 import javax.websocket.DeploymentException;
 
 import org.glassfish.tyrus.client.ClientManager;
 
-import util.ConstantsTest;
+import util.Constants;
 
 public class ClientThread implements Runnable {
-  
+
   public static volatile boolean isClosing = false;
   private String uri;
-  
+
   public ClientThread(String uri) {
     this.uri = uri;
   }
-  
+
   @Override
   public void run() {
     ClientManager client = ClientManager.createClient();
     try {
-      client.connectToServer(ClientWebSocket.class, new URI(ConstantsTest.PROTOCOL + uri + ':'
-          + ConstantsTest.PORT + ConstantsTest.LINK + ConstantsTest.ENDPOINT));
+      client.connectToServer(ClientWebSocket.class, new URI(
+          Constants.PROTOCOL + uri + ':' + Constants.PORT + Constants.LINK + Constants.ENDPOINT));
     } catch (DeploymentException | URISyntaxException e) {
       throw new RuntimeException(e);
     }
