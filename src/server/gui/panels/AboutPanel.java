badf72af9d4661e36378cb5ddba70863d73296d6
Cephas Mensah <carmstr7@asu.edu>
2018-04-05 18:47:08 -0700

    adding in AboutPanel.java

---
+++ src/server/gui/panels/AboutPanel.java
@@ -0,0 +1,55 @@
+package server.gui.panels;
+
+import java.awt.Color;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+
+import server.gui.EmotivComposer;
+import server.gui.actions.MouseEvents;
+
+public class AboutPanel extends JDialog {
+  private ClassLoader loader = getClass().getClassLoader();
+  private EmotivComposer parent;
+  private JPanel contentPane;
+  private JScrollPane scrollPane;
+  private JLabel aboutPanel;
+
+  public AboutPanel(EmotivComposer parent) {
+    super(parent);
+    this.parent = parent;
+    setUndecorated(true);
+    setSize(350, 450);
+    setLocationRelativeTo(parent);
+
+    contentPane = new JPanel();
+    contentPane.setBackground(Color.WHITE);
+    contentPane.setLayout(null);
+    setContentPane(contentPane);
+
+    initialize();
+  }
+
+  private void initialize() {
+    scrollPane = new JScrollPane();
+    scrollPane.setBackground(Color.WHITE);
+    scrollPane.setBounds(5, 5, 340, 440);
+
+    aboutPanel = new JLabel(
+        "<html><body><h2>Project 3 - SER 516</h2><div><p><h3>Scrum Master</h3> Patrik Bartakke</p><p><h3>Development Teams</h3></p><p><h3>Client Side</h3></p><ul><li>Chetanya Ahuja</li><li>Shibani Arvind Hegde</li><li>Nikita Bahl</li><li>Sanchari Banerjee</li><li>Jahnavi Bantupalli</li><li>Karansher Bhangal</li><li>Kritika Bhat</li><li>Shilpa Bhat</li><li>Debarati Bhattacharyya</li></ul><p><h3>Server Side</h3></p><ul><li>Cephas Armstrong-Mensah</li><li>Vihar Bhatt</li><li>Zelin Bao</li> </ul></div></body></html>");
+    aboutPanel.setVerticalAlignment(SwingConstants.TOP);
+
+    scrollPane.setViewportView(aboutPanel);
+    contentPane.add(scrollPane);
+
+    setVisible(true);
+    addMouseListener(new MouseEvents(this, "about"));
+  }
+
+  public void disposeScrollPane() {
+    dispose();
+  }
+}
